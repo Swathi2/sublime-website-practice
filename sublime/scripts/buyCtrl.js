647506fb1e024102d605151dae50c9aac55fc082
Swathi <swathireddy.patlolla@gmail.com>
2018-01-04 09:45:27 -0500

    added code in buyctrl

--- sublime/scripts/buyCtrl.js
+++ sublime/scripts/buyCtrl.js
@@ -1,10 +1,12 @@
-﻿'use strict';
+'use strict';
 angular.module('sublimeApp')
 .controller('buyCtrl', function ($scope) {
     var vm = this;
     $scope.hidelicenses = false;
     $scope.licenseKey = true;
+    $scope.disabled = true;
     $scope.bsnssLicenseFunc = function () {
+        $scope.disabled = false;
         $scope.hidelicenses = true;
         $scope.licenseKey = false;
     }
